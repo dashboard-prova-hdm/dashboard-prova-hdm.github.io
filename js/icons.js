3cdfbae2fd12c0b18d6c5f69291b05c3119682aa
dashboard-prova-hdm <78870921+dashboard-prova-hdm@users.noreply.github.com>
2021-03-12 11:41:32 +0100

    telegram

--- js/icons.js
+++ js/icons.js
@@ -44,29 +44,6 @@ twitterShare.onclick = function(e) {
 }
 
 
-// ---- TELEGRAM ---- //
-
-var telegramShare = document.querySelector('[data-js="telegram-share"]');
-
-telegramShare.onclick = function(e) {
-    e.preventDefault();
-    var top = window.screen.height - 500;
-    top = top > 0 ? top / 2 : 0;
-
-    var left = window.screen.width - 500;
-    left = left > 0 ? left / 2 : 0;
-
-    var uploadWin = window.open('https://t.me/share/url?url=' + document.URL, "Upload Chapter content", "width=500,height=500" + ",top=" + top + ",left=" + left + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
-    uploadWin.moveTo(left, top);
-    uploadWin.focus();
-
-    if (mailWindow.focus) {
-        mailWindow.focus();
-    }
-    return false;
-}
-
-
 // ---- LINKEDIN ---- //
 
 var linkedinShare = document.querySelector('[data-js="linkedin-share"]');
